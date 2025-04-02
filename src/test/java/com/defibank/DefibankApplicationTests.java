8f2fe96fc694eb32f2186deee59b8c41d69b01ec
carolinaoliveirasilveira <carolinaoliveirasilveira@outlook.com>
2025-04-02 15:53:47 -0300

    Initial commit

---
+++ src/test/java/com/defibank/DefibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
